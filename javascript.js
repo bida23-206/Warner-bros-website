b16ab64b8a89f0dd6682fdf6646b9e04f65a96d3
bida23-206 <166514971+bida23-206@users.noreply.github.com>
2024-05-23 09:03:03 +0200

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,12 @@
+const arrows = document.querySelectorAll(".arrow"); const movielists = document.querySelectorAll(".movie-list");
+
+arrows.forEach((arrow, i) => {
+  arrow.addEventListener("click", () => {
+    movielists[i].style.transform = `translateX(${
+      movielists[i].computedStyleMap().get("transform")[0].x.value - 300}px)`;
+  });
+});
+
+
+
+
